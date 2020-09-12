3415249067ad02902d81a66464a945ac134e360b
Justin Clifford <justoon7.jc@gmail.com>
2020-09-12 02:52:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(numbers)
   counter = 0
   new_numbers = []
   while counter < numbers.length do
-    numbers[counter]**2)
+    numbers[counter]**2
     counter += 1
     new_numbers.push(numbers[counter])
   end
